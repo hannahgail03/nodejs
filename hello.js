c9a595acfac4a8e858042b92117fdead9a2095b6
hannahgail03 <hgfajutag@miu.edu>
2021-07-09 09:06:49 -0500

    Update hello.js

--- hello.js
+++ hello.js
@@ -6,12 +6,12 @@ const readline = require('readline').createInterface({
 readline.question('What is your name? ', name => {
     console.log(`Welcome ${name}`);
   	  readline.question('What is your age?', age => {
-        const allowed = 'You\'re allowed to get a drivers license in Iowa';
-        const notAllowed = 'You\'re not allowed to drive in Iowa';
-        if(age<=16)
-        console.log(notAllowed)
-        else
-        console.log(allowed);
+        const allowed = "You're allowed to get a drivers license in Iowa";
+        const notAllowed = "You're not allowed to drive in Iowa";
+        if(age<=16){
+        console.log(notAllowed); }
+        else{
+        console.log(allowed);}
         readline.close();
     });
 });
